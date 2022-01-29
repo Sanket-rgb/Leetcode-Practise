8e2af30738822dca39465bac0112f5ac5623705e
Sanket Koli <67689067+Sanket-rgb@users.noreply.github.com>
2022-01-29 10:54:59 -0500

    Time: 32 ms, Memory: 13.9 MB - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -13,22 +13,26 @@ class Solution:
         if N == 1: return False
         
         for item in s:
+            if item == "(":
+                stack.append(")")
+            elif item == "[":
+                stack.append("]")
+            elif item == "{":
+                stack.append("}")
+            elif len(stack) == 0 or stack.pop() != item:
+                return False
+        return not len(stack)
+    # True if len(stack) == 0 else False
+#         for item in s:
             
-            if item == "(" or item == "[" or item == "{":
-                stack.append(item)
-            elif item == ")" or item == "]" or item == "}":
-                if len(stack) == 0: return False
-                if stack.pop() != dic[item]:
-                    return False
-            count += 1
-        return True if len(stack) == 0 or count == N else False
+#             if item == "(" or item == "[" or item == "{":
+#                 stack.append(item)
+#             elif item == ")" or item == "]" or item == "}":
+#                 if len(stack) == 0: return False
+#                 if stack.pop() != dic[item]:
+#                     return False
+#             count += 1
+#         return False if len(stack) != 0 else True
     
-    # for item in s:
-    #         if s == "(":
-    #             stack.append(")")
-    #         elif s == "[":
-    #             stack.append("]")
-    #         elif s == "{":
-    #             stack.append("}")
-    #         elif len(stack) == 0 or stack.pop() != item
+    
         
