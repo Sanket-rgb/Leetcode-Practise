1bcef325a5f31f051f3c84b29cda5da4c6e3fd8a
Sanket Koli <67689067+Sanket-rgb@users.noreply.github.com>
2022-02-16 18:25:47 -0500

    Time: 38 ms (66.48%), Space: 13.9 MB (69.54%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        hset = set()
+        
+        while n not in hset:
+            hset.add(n)
+            
+            if n == 1:
+                return True
+            else:
+                n = self.sq_sum(n)
+        else:
+            return False
+        
+    def sq_sum(self, n):
+        s = 0
+        for i in str(n):
+            s += int(i)**2
+        return s
+        
+    
+      
