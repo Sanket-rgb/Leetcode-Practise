6c53560066f51c90c4dea5c4d41426f74ffb8fe0
Sanket Koli <67689067+Sanket-rgb@users.noreply.github.com>
2022-02-16 19:55:03 -0500

    Time: 95 ms (58.47%), Space: 15.1 MB (59.01%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -5,9 +5,17 @@ class Solution:
         hashmap = {}
         
         for i in range(N):
-            for j in range(i+1, N):
-                if nums[j] == target - nums[i]:
-                    return [i, j]
+            hashmap[nums[i]] = i
+        
+        for i in range(N):
+            c = target - nums[i]
+            
+            if c in hashmap and hashmap[c] != i:
+                return [i, hashmap[c]]
+        # for i in range(N):
+        #     for j in range(i+1, N):
+        #         if nums[j] == target - nums[i]:
+        #             return [i, j]
         
         
 #         for i in nums:
