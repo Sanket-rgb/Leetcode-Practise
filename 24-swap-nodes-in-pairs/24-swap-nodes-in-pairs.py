80bca34eeaa1d8b7c1e6afd80a4027dacb17787c
Sanket Koli <67689067+Sanket-rgb@users.noreply.github.com>
2022-04-04 05:08:34 +0530

    Time: 36 ms (76.40%), Space: 14 MB (20.47%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -20,24 +20,3 @@ class Solution:
             prev = curr
             curr = nextPair
         return dummy.next
-            
-#         if head is None:
-#             return
-        
-        
-#         curr = head
-#         while curr.next is not None:
-#             curr.next = curr.next.next
-#             curr.next.next = curr
-#             if curr.next is not None:
-#                 curr = curr.next
-#         return head
-        # while curr.next is not None:
-        #     curr.val, curr.next.val = curr.next.val, curr.val
-        #     if curr.next.next is not None:
-        #         curr = curr.next.next
-        # return head
-        # if head.next.next:
-        #     head.val, head.next.val = head.next.val, head.val
-        #     head = head.next.next
-        #     self.swapPairs(head)
