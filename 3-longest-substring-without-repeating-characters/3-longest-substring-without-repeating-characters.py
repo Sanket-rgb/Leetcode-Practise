b253913ea928b57ff44b72b17235487511c259aa
Sanket Koli <67689067+Sanket-rgb@users.noreply.github.com>
2022-04-12 08:25:01 +0530

    Time: 72 ms (78.80%), Space: 14 MB (94.15%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -11,22 +11,6 @@ class Solution:
             charSet.add(s[r])
             res = max(res, r-l+1)
         return res
-#         substring = ""
-#         longest_substring = ""
-#         i = 0
-        
-#         for j in range(len(s)):
-#             if s[j] not in s[i:j]:
-#                 pass
-#             else:
-#                 substring = s[i:j]
-#                 if len(substring) > len(longest_substring):
-#                     longest_substring = substring
-#                 i = j
-        
-#         if len(substring) == 0 or len(longest_substring) == 0:
-#             return len(s)
-#         else:
-#             return len(longest_substring)
+
 
                 
